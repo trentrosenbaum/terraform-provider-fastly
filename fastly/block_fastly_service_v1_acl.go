28bbc30a29342d8867ec2b13d9fefc0544e4046d
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-09 17:35:53 +0100

    Migrated ACL schema

---
+++ fastly/block_fastly_service_v1_acl.go
@@ -0,0 +1,29 @@
+package fastly
+
+import (
+	"fmt"
+	gofastly "github.com/fastly/go-fastly/fastly"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"log"
+)
+
+var aclSchema = &schema.Schema{
+	Type:     schema.TypeSet,
+	Optional: true,
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			// Required fields
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Unique name to refer to this ACL",
+			},
+			// Optional fields
+			"acl_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Generated acl id",
+			},
+		},
+	},
+}
