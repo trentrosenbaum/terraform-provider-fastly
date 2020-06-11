d8e454613b92197044510456ce438e28574a90d1
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-11 14:38:06 +0100

    Migrated dynamicsnippet to attribute handler

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -21,7 +21,7 @@ var condition 			= NewServiceCondition()
 var dictionary 			= NewServiceDictionary()
 var director 			= NewServiceDirector()
 var domain 				= NewServiceDomain()
-
+var dynamicsnippet 		= NewServiceDynamicSnippet()
 
 func resourceServiceV1() *schema.Resource {
 	return &schema.Resource{
@@ -122,9 +122,9 @@ func resourceServiceV1() *schema.Resource {
 			"logging_sftp":          sftpSchema,
 			"response_object":    responseobjectSchema,
 			"request_setting":    requestsettingSchema,
-			"vcl": vclSchema,
-			"snippet":        snippetSchema,
-			"dynamicsnippet": dynamicsnippetSchema,
+			"vcl":                vclSchema,
+			"snippet":            snippetSchema,
+			dynamicsnippet.GetKey():     dynamicsnippet.GetSchema(),
 			acl.GetKey():         acl.GetSchema(),
 			dictionary.GetKey():         dictionary.GetSchema(),
 		},
@@ -201,7 +201,7 @@ func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
 		"request_setting",
 		cachesetting.GetKey(),
 		"snippet",
-		"dynamicsnippet",
+		dynamicsnippet.GetKey(),
 		"vcl",
 		acl.GetKey(),
 		dictionary.GetKey(),
@@ -436,8 +436,8 @@ func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
 				return err
 			}
 		}
-		if d.HasChange("dynamicsnippet") {
-			if err := processDynamicSnippet(d, conn, latestVersion); err != nil {
+		if d.HasChange(dynamicsnippet.GetKey()) {
+			if err := dynamicsnippet.Process(d, latestVersion, conn); err != nil {
 				return err
 			}
 		}
@@ -609,7 +609,7 @@ func resourceServiceV1Read(d *schema.ResourceData, meta interface{}) error {
 		if err := readSnippet(conn, d, s); err != nil {
 			return err
 		}
-		if err := readDynamicSnippet(conn, d, s); err != nil {
+		if err := dynamicsnippet.Read(d, s, conn); err != nil {
 			return err
 		}
 		if err := cachesetting.Read(d, s, conn); err != nil {
