a60a4e6addeb3fdaaa49994dfda905505e69488b
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-12 09:01:47 +0100

    Factored out settings (default_host && default_ttl)

--- fastly/base_fastly_service_v1.go
+++ fastly/base_fastly_service_v1.go
@@ -8,10 +8,11 @@ import (
 // SERVICE ATTRIBUTE
 
 type ServiceAttributeDefinition interface {
-	GetKey() string
 	Read(d *schema.ResourceData, s *fastly.ServiceDetail, conn *fastly.Client) error
 	Process(d *schema.ResourceData, latestVersion int, conn *fastly.Client) error
 	Register(d *schema.Resource) error
+	HasChange(d *schema.ResourceData) bool
+	MustProcess(d *schema.ResourceData, initialVersion bool) bool
 }
 
 type DefaultServiceAttributeHandler struct {
@@ -21,4 +22,13 @@ type DefaultServiceAttributeHandler struct {
 func (h *DefaultServiceAttributeHandler) GetKey() string {
 	return h.key
 }
+
+func (h *DefaultServiceAttributeHandler) HasChange(d *schema.ResourceData) bool {
+	return d.HasChange(h.key)
+}
+
+func (h *DefaultServiceAttributeHandler) MustProcess(d *schema.ResourceData, initialVersion bool) bool {
+	return h.HasChange(d)
+}
+
 
