762c8e6e87e182324f5d2ab9cbe1acc697549a74
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-30 08:05:03 +0100

    Working basic Wasm test

--- fastly/resource_fastly_service_wasm_v1_test.go
+++ fastly/resource_fastly_service_wasm_v1_test.go
@@ -13,10 +13,7 @@ import (
 func TestAccFastlyServiceWASMV1_basic(t *testing.T) {
 	var service gofastly.ServiceDetail
 	name := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
-	comment := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
-	versionComment := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
 	domainName1 := fmt.Sprintf("fastly-test1.tf-%s.com", acctest.RandString(10))
-	domainName2 := fmt.Sprintf("fastly-test2.tf-%s.com", acctest.RandString(10))
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -26,7 +23,7 @@ func TestAccFastlyServiceWASMV1_basic(t *testing.T) {
 			{
 				Config: testAccServiceWASMV1Config(name, domainName1),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckServiceWASMV1Exists("fastly_service_wasm_v1.foo", &service),
+					testAccCheckServiceV1Exists("fastly_service_wasm_v1.foo", &service),
 					resource.TestCheckResourceAttr(
 						"fastly_service_wasm_v1.foo", "name", name),
 					resource.TestCheckResourceAttr(
@@ -35,26 +32,6 @@ func TestAccFastlyServiceWASMV1_basic(t *testing.T) {
 						"fastly_service_wasm_v1.foo", "version_comment", ""),
 					resource.TestCheckResourceAttr(
 						"fastly_service_wasm_v1.foo", "active_version", "0"),
-					resource.TestCheckResourceAttr(
-						"fastly_service_wasm_v1.foo", "domain.#", "1"),
-					resource.TestCheckResourceAttr(
-						"fastly_service_wasm_v1.foo", "backend.#", "1"),
-					resource.TestCheckResourceAttr(
-						"fastly_service_wasm_v1.foo", "package.#", "1"),
-				),
-			},
-			{
-				Config: testAccServiceWASMV1Config_Update(name, comment, versionComment, domainName2),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckServiceWASMV1Exists("fastly_service_wasm_v1.foo", &service),
-					resource.TestCheckResourceAttr(
-						"fastly_service_wasm_v1.foo", "name", name),
-					resource.TestCheckResourceAttr(
-						"fastly_service_wasm_v1.foo", "comment", comment),
-					resource.TestCheckResourceAttr(
-						"fastly_service_wasm_v1.foo", "version_comment", versionComment),
-					resource.TestCheckResourceAttr(
-						"fastly_service_wasm_v1.foo", "active_version", "1"),
 					resource.TestCheckResourceAttr(
 						"fastly_service_wasm_v1.foo", "domain.#", "1"),
 					resource.TestCheckResourceAttr(
@@ -89,32 +66,6 @@ func testAccCheckServiceWASMV1Destroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccCheckServiceWASMV1Exists(n string, service *gofastly.ServiceDetail) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Not found: %s", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No Service ID is set")
-		}
-
-		conn := testAccProvider.Meta().(*FastlyClient).conn
-		latest, err := conn.GetServiceDetails(&gofastly.GetServiceInput{
-			ID: rs.Primary.ID,
-		})
-
-		if err != nil {
-			return err
-		}
-
-		*service = *latest
-
-		return nil
-	}
-}
-
 func testAccServiceWASMV1Config(name, domain string) string {
 	return fmt.Sprintf(`
 resource "fastly_service_wasm_v1" "foo" {
@@ -134,26 +85,4 @@ resource "fastly_service_wasm_v1" "foo" {
   force_destroy = true
   activate = false
 }`, name, domain)
-}
-
-func testAccServiceWASMV1Config_Update(name, comment, versionComment, domain string) string {
-	return fmt.Sprintf(`
-resource "fastly_service_wasm_v1" "foo" {
-  name    = "%s"
-  comment = "%s"
-  version_comment = "%s"
-  domain {
-    name    = "%s"
-    comment = "tf-testing-domain"
-  }
-  backend {
-    address = "test.google.com"
-    name    = "google"
-  }
-  package {
-    filename = "test_fixtures/package/valid.tar.gz"
-	source_code_hash = filesha512("test_fixtures/package/valid.tar.gz")
-  }
-  force_destroy = true
-}`, name, comment, versionComment, domain)
 }
