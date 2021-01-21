24dd3a8df525168bbec8cbc06cbd450a5854583b
Ben Gesoff <ben.gesoff@opencredo.com>
2021-01-21 12:42:12 +0000

    Add test to update certificate for TLS activation

--- fastly/resource_fastly_tls_activation_test.go
+++ fastly/resource_fastly_tls_activation_test.go
@@ -13,10 +13,14 @@ import (
 
 func TestAccFastlyTLSActivationBasic(t *testing.T) {
 	domain := fmt.Sprintf("tf-test-%s.com", acctest.RandString(10))
-	key, cert, err := generateKeyAndCert(domain)
+	key, cert, cert2, err := generateKeyAndMultipleCerts(domain)
 	require.NoError(t, err)
 	key = strings.ReplaceAll(key, "\n", `\n`)
 	cert = strings.ReplaceAll(cert, "\n", `\n`)
+	cert2 = strings.ReplaceAll(cert2, "\n", `\n`)
+
+	name := fmt.Sprintf("tf-test-%s", acctest.RandString(20))
+	updatedName := fmt.Sprintf("tf-test-%s", acctest.RandString(20))
 
 	resourceName := "fastly_tls_activation.test"
 	resource.ParallelTest(t, resource.TestCase{
@@ -25,7 +29,7 @@ func TestAccFastlyTLSActivationBasic(t *testing.T) {
 		CheckDestroy: testAccFastlyTLSActivationCheckDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccFastlyTLSActivationBasicConfig(key, cert, domain),
+				Config: testAccFastlyTLSActivationBasicConfig(name, name, key, name, cert, domain),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(resourceName, "certificate_id"),
 					resource.TestCheckResourceAttrSet(resourceName, "configuration_id"),
@@ -34,19 +38,21 @@ func TestAccFastlyTLSActivationBasic(t *testing.T) {
 					testAccFastlyTLSActivationCheckExists(resourceName),
 				),
 			},
+			{
+				Config: testAccFastlyTLSActivationBasicConfig(name, name, key, updatedName, cert2, domain),
+				Check:  testAccFastlyTLSActivationCheckExists(resourceName),
+			},
 		},
 	})
 }
 
-func testAccFastlyTLSActivationBasicConfig(key, cert, domain string) string {
-	name := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
-
+func testAccFastlyTLSActivationBasicConfig(serviceName, keyName, key, certName, cert, domain string) string {
 	return fmt.Sprintf(`
 resource "fastly_service_v1" "test" {
   name = "%s"
 
   domain {
-    name    = "%s"
+    name = "%s"
   }
 
   backend {
@@ -73,7 +79,7 @@ resource "fastly_tls_activation" "test" {
   domain = "%s"
   depends_on = [fastly_service_v1.test]
 }
-`, name, domain, key, name, cert, name, domain)
+`, serviceName, domain, key, keyName, cert, certName, domain)
 }
 
 func testAccFastlyTLSActivationCheckExists(resourceName string) resource.TestCheckFunc {
