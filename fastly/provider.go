76f5ea60d8ce1daf7d3ea8e7a57eaeb4fc6ddb37
Ben Gesoff <ben.gesoff@opencredo.com>
2021-01-26 16:56:46 +0000

    TLS Subscription Validation Resource

--- fastly/provider.go
+++ fastly/provider.go
@@ -49,6 +49,7 @@ func Provider() terraform.ResourceProvider {
 			"fastly_tls_private_key":                    resourceTLSPrivateKey(),
 			"fastly_tls_platform_certificate":           resourceTLSPlatformCertificate(),
 			"fastly_tls_subscription":                   resourceFastlyTLSSubscription(),
+			"fastly_tls_subscription_validation":        resourceFastlyTLSSubscriptionValidation(),
 			"fastly_user_v1":                            resourceUserV1(),
 		},
 	}
